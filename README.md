d625e1a71c962a2d937d55bf59155a0c878cfffa
Mayank Grover <mayank.grover@kartrocket.com>
2016-07-18 18:25:51 +0530

    Initialization

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cakephp_social_login
-CakePHP Social Login provides your application ability to login with third party apps like facebook twitter google and linkedin
+# laravel_social_login
+Laravel Social Login provides your application ability to login with third party apps like facebook twitter google and linkedin
--- src/library/facebook/FbRedirectLoginHelper.php
+++ src/library/facebook/FbRedirectLoginHelper.php
@@ -3,9 +3,9 @@ class FbRedirectLoginHelper extends \Facebook\FacebookRedirectLoginHelper {
 	var $sessionHandler = null;
 
 	protected function storeState($state) {
-		$this->sessionHandler->write('state', $state);
+		$this->sessionHandler->put('state', $state);
 	}
 	protected function loadState() {
-		return $this->state = $this->sessionHandler->read('state');
+		return $this->state = $this->sessionHandler->get('state');
 	}
 }
